eca3a52f6594fe0d4a109d7bb91e891fc18dfb53
gaurav bisht <gauravengg.bisht@gmail.com>
2024-02-02 10:28:02 +0530

    login form sign up form courseCard exploreMore error page and utils are added

---
+++ src/pages/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Error = () => {
+  return (
+    <div className='justify-center items-center text-3xl '>
+        <h1>Page not found 404</h1>
+    </div>
+  )
+}
+
+export default Error;
