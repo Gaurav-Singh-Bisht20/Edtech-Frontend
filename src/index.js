e66d9272182d1e802e2e3084eefaa90d16dd7f36
gaurav bisht <gauravengg.bisht@gmail.com>
2024-02-05 12:04:35 +0530

    dashboard added with its setting and other functionlity slices and apis are also updated

--- src/index.js
+++ src/index.js
@@ -3,23 +3,26 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
-import rootReducer from "./reducer/index"
+import rootReducer from "./reducer";
+import {configureStore} from "@reduxjs/toolkit"
 import { Toaster } from "react-hot-toast";
+
 
 const store = configureStore({
   reducer:rootReducer,
-})
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+  <Provider store = {store}>
     <BrowserRouter>
-    <App />
-    <Toaster />
-    </BrowserRouter>
-    </Provider>
+        <App />
+        <Toaster/>
+      </BrowserRouter>
+  </Provider>
+    
+    
   </React.StrictMode>
 );
