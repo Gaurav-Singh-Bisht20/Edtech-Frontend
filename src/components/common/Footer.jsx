49a3b3a0e2b14b1aac4d42dfcb88deed5ee7ce3f
gaurav bisht <gauravengg.bisht@gmail.com>
2024-02-14 12:33:23 +0530

    some minor changes and code reviewd

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -2,10 +2,7 @@ import React from "react";
 import { FooterLink2 } from "../../data/footer-links";
 import { Link } from "react-router-dom";
 
-// Images
 import Logo from "../../assets/Logo/Logo-Full-Light.png";
-
-// Icons
 import { FaFacebook, FaGoogle, FaTwitter, FaYoutube } from "react-icons/fa";
 
 const BottomFooter = ["Privacy Policy", "Cookie Policy", "Terms"];
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from "react-redux"
 import { Outlet } from "react-router-dom"
 
-import Sidebar from "../components/core/Dashboard/Sidebar"
+import Sidebar from "../components/core/Dashboard/Settings/Sidebar"
 
 function Dashboard() {
   const { loading: profileLoading } = useSelector((state) => state.profile)
