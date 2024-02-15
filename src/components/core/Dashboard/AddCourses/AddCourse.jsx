c452f214b69321a6b0c058f3ff8be4f81e6aa469
gaurav bisht <gauravengg.bisht@gmail.com>
2024-02-15 11:46:40 +0530

    courseDetailsAPI is added , addCourse's 1 step is almost completed

--- src/components/core/Dashboard/AddCourses/AddCourse.jsx
+++ src/components/core/Dashboard/AddCourses/AddCourse.jsx
@@ -1,30 +1,29 @@
-import React from 'react'
-import RenderSteps from './RenderSteps'
+import RenderSteps from "./RenderSteps"
 
-const AddCourse = () => {
-  return (
-    <div className='text-white'>
-        <div>
-            <h1>Add courses</h1>
-            <div>
-                <RenderSteps/>
+export default function AddCourse() {
+    return (
+        <>
+            <div className="text-white">
+                <div>
+                    <h1>Add Course</h1>
+                    <div>
+                        <RenderSteps />
+                    </div>
+                </div>
+                <div>
+                    <p>Code Upload Tips</p>
+                    <ul>
+                        <li>Set the Course Price option or make it free.</li>
+                        <li>Standard size for the course thumbnail is 1024x576.</li>
+                        <li>Video section controls the course overview video.</li>
+                        <li>Set the Course Price option or make it free.</li>
+                        <li>Standard size for the course thumbnail is 1024x576.</li>
+                        <li>Video section controls the course overview video.</li>
+                        <li>Set the Course Price option or make it free.</li>
+                        <li>Standard size for the course thumbnail is 1024x576.</li>
+                    </ul>
+                </div>
             </div>
-        </div>
-        <div>
-            <p>Code Upload Tips</p>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-                <li>4</li>
-                <li>5</li>
-                <li>6</li>
-                <li>7</li>
-                <li>8</li>
-            </ul>
-        </div>
-    </div>
-  )
+        </>
+    )
 }
-
-export default AddCourse
