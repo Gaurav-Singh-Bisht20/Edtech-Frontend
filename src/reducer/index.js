62d3f79e7d7fb96ddd7b06e1baa4faf90f5c44c5
gaurav bisht <gauravengg.bisht@gmail.com>
2024-03-23 11:05:52 +0530

    navbar links updated

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,4 +1,4 @@
-import {combineReducers, createReducer} from "@reduxjs/toolkit";
+import {combineReducers} from "@reduxjs/toolkit";
 import authReducer from "../slices/authSlice";
 import profileReducer from "../slices/profileSlice";
 import cartReducer from "../slices/cartSlice";
